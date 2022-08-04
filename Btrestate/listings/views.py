9037a99073b7cd04241d8c77ef2aeab97b885d56
Rapheal Oke-David <raphealoke-david@Rapheals-MacBook-Air.local>
2022-08-04 18:43:39 +0200

    added models, views method display and search

---
+++ Btrestate/listings/views.py
@@ -0,0 +1,19 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+from listings.models import Listing
+
+# Create your views here.
+def index(request):
+    listings = Listing.objects.all()
+    
+    context ={
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html')
+
+def search(request):
+    return render(request, 'listings/search.html')
